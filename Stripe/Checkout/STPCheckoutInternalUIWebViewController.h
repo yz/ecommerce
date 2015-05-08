7424a08e44501369514ef7239e26f75afe44a9fe
ashwinkarthik <ashwinkarthik.srinivasan@stonybrook.edu>
2015-05-08 10:36:03 -0400

    added Stripe

---
+++ Stripe/Checkout/STPCheckoutInternalUIWebViewController.h
@@ -0,0 +1,34 @@
+//
+//  STPCheckoutInternalUIWebViewController.h
+//  Stripe
+//
+//  Created by Jack Flintermann on 1/7/15.
+//  Copyright (c) 2015 Stripe, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#if TARGET_OS_IPHONE
+
+#import <UIKit/UIKit.h>
+#import "STPCheckoutDelegate.h"
+#import "STPCheckoutViewController.h"
+
+@interface STPCheckoutInternalUIWebViewController : UIViewController<STPCheckoutDelegate, UIScrollViewDelegate>
+
+- (instancetype)initWithCheckoutViewController:(STPCheckoutViewController *)checkoutViewController;
+
+@property (weak, nonatomic, readonly) STPCheckoutViewController *checkoutController;
+@property (weak, nonatomic) UIView *webView;
+@property (nonatomic) STPIOSCheckoutWebViewAdapter *adapter;
+@property (weak, nonatomic) UIActivityIndicatorView *activityIndicator;
+@property (nonatomic) STPCheckoutOptions *options;
+@property (nonatomic) NSURL *logoURL;
+@property (nonatomic) NSURL *url;
+@property (nonatomic, weak) id<STPCheckoutViewControllerDelegate> delegate;
+@property (nonatomic) BOOL backendChargeSuccessful;
+@property (nonatomic) NSError *backendChargeError;
+
+@end
+
+#endif
